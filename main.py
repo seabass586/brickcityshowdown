7e4e6b2283b44628255b021efa5d6abde9801349
Jack Hunsberger <jack.hunsberger2002@gmail.com>
2022-03-05 16:02:15 -0500

    testing github

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+from sys import exit
+import pygame.locals
+
+pygame.init()
+
+# create the base screen
+screen = pygame.display.set_mode((800, 600))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                exit()
+

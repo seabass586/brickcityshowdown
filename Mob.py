9b88754a0932fc3fbd45b02db7cd4f9089e57600
Jack Hunsberger <jack.hunsberger2002@gmail.com>
2022-03-05 18:03:21 -0500

    kept mobs inside the game box

--- Mob.py
+++ Mob.py
@@ -5,18 +5,18 @@ import random
 class Mob(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((30, 40))
+        self.image = pygame.Surface((20, 20))
         self.image.fill(shooter.RED)
         self.rect = self.image.get_rect()
-        self.rect.x = random.randrange(shooter.WIDTH - self.rect.width)
-        self.rect.y = random.randrange(-100, -40)
-        self.speedy = random.randrange(1, 8)
+        self.rect.x = random.randrange(490, 790 - self.rect.width)
+        self.rect.y = 390
+        self.speedy = random.randrange(1, 4)
         self.speedx = random.randrange(-3, 3)
 
     def update(self):
         self.rect.x += self.speedx
         self.rect.y += self.speedy
         if self.rect.top > shooter.HEIGHT + 10 or self.rect.left < -25 or self.rect.right > shooter.WIDTH + 20:
-            self.rect.x = random.randrange(shooter.WIDTH - self.rect.width)
-            self.rect.y = random.randrange(-100, -40)
-            self.speedy = random.randrange(1, 8)
+            self.rect.x = random.randrange(490, 790 - self.rect.width)
+            self.rect.y = 390
+            self.speedy = random.randrange(1, 4)

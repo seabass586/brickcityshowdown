3adeb944f90009c73c31b3f1be907fe5b4bf9aa5
Jack Hunsberger <jack.hunsberger2002@gmail.com>
2022-03-05 16:04:26 -0500

    added shooter.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ pygame.init()
 # create the base screen
 screen = pygame.display.set_mode((800, 600))
 
+# basic game loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:

fe7c9ba2c7168eb3fa023d3bb519cd96f61cd913
SteveWu <86623538+SteveWufeng@users.noreply.github.com>
2022-03-05 16:31:38 -0500

    modulized code

--- shooter.py
+++ shooter.py
@@ -1,6 +1,6 @@
 import pygame
 import Mob
-import Player
+import Player 
 import Bullet
 import random
 import os
@@ -11,10 +11,6 @@ WIDTH = 500
 HEIGHT = 768
 FPS = 60
 
-pygame.init()
-pygame.mixer.init()
-pygame.font.init()
-
 # define colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -23,56 +19,64 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 YELLOW = (255, 255, 0)
 
-# initialize pygame and create window
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("BrickBreaker")
-clock = pygame.time.Clock()
+def main():
+    pygame.init()
+    pygame.mixer.init()
+    pygame.font.init()
 
-all_sprites = pygame.sprite.Group()
-mobs = pygame.sprite.Group()
-bullets = pygame.sprite.Group()
-player = Player()
-all_sprites.add(player)
-for i in range(8):
-    m = Mob()
-    all_sprites.add(m)
-    mobs.add(m)
+    # initialize pygame and create window
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("BrickBreaker")
+    clock = pygame.time.Clock()
 
-# Game loop
-running = True
+    all_sprites = pygame.sprite.Group()
+    mobs = pygame.sprite.Group()
+    bullets = pygame.sprite.Group()
+    player = Player.Player()
+    all_sprites.add(player)
+    for i in range(8):
+        m = Mob.Mob()
+        all_sprites.add(m)
+        mobs.add(m)
 
-while running:
-    # keep loop running at right speed
-    clock.tick(FPS)
+    # Game loop
+    running = True
 
-    # process input(events)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    while running:
+        # keep loop running at right speed
+        clock.tick(FPS)
+
+        # process input(events)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    player.shoot()
+
+        # update
+        all_sprites.update()
+        mobs.update()
+
+        # check to see if a mob hit the player
+        hits = pygame.sprite.spritecollide(player, mobs, False)
+        if hits:
             running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                player.shoot()
 
-    # update
-    all_sprites.update()
-    mobs.update()
+        # draw/render
+        screen.fill(BLACK)
+        all_sprites.draw(screen)
 
-    # check to see if a mob hit the player
-    hits = pygame.sprite.spritecollide(player, mobs, False)
-    if hits:
-        running = False
+        # text
+        score = 0;
+        myfont = pygame.font.SysFont('Comic Sans MS', 30)
+        textsurface = myfont.render('Score: ' + str(score), False, WHITE)
+        screen.blit(textsurface, (0, 0))
 
-    # draw/render
-    screen.fill(BLACK)
-    all_sprites.draw(screen)
+        # after drawing everything, flip the display
+        pygame.display.flip()
 
-    # text
-    score = 0;
-    myfont = pygame.font.SysFont('Comic Sans MS', 30)
-    textsurface = myfont.render('Score: ' + str(score), False, WHITE)
-    screen.blit(textsurface, (0, 0))
+    pygame.quit()
 
-    # after drawing everything, flip the display
-    pygame.display.flip()
-
-pygame.quit()
+if __name__ == "__main__":
+    main()

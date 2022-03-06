108b09b4863ac71a22ebed93b88c95dca2a929d5
SteveWu <86623538+SteveWufeng@users.noreply.github.com>
2022-03-06 04:31:56 -0500

    added more pages for help

--- Story.py
+++ Story.py
@@ -23,3 +23,20 @@ def storyScreen():
                 wait_time = 0
         wait_time -= 1
         sleep(0.01)
+    
+def controlHelpScreen():
+    while True:
+        pygame.init()
+        pygame.display.set_caption("Story Of the Game")
+        res = (shooter.WIDTH, shooter.HEIGHT)
+        screen = pygame.display.set_mode(res) 
+        bg = pygame.image.load("assets/ControlHelp.png")
+        new = pygame.transform.scale(bg, (1280, 720))
+        screen.blit(new, (0, 0))
+        pygame.display.update()
+    
+        # next page
+        for ev in pygame.event.get():     
+            if ev.type == pygame.KEYDOWN:
+                if ev.key == pygame.K_RIGHT:
+                    

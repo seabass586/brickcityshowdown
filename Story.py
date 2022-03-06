e49f2a4a96bcc8b7293e32122f29bb860d4222f2
SteveWu <86623538+SteveWufeng@users.noreply.github.com>
2022-03-06 04:41:27 -0500

    added help screen

--- Story.py
+++ Story.py
@@ -30,7 +30,28 @@ def controlHelpScreen():
         pygame.display.set_caption("Story Of the Game")
         res = (shooter.WIDTH, shooter.HEIGHT)
         screen = pygame.display.set_mode(res) 
-        bg = pygame.image.load("assets/ControlHelp.png")
+        bg = pygame.image.load("assets/controls.png")
+        new = pygame.transform.scale(bg, (1280, 720))
+        screen.blit(new, (0, 0))
+        pygame.display.update()
+    
+        # navigate through pages
+        for ev in pygame.event.get():     
+            if ev.type == pygame.KEYDOWN:
+                if ev.key == pygame.K_RIGHT:
+                    if (storyHelpScreen()):
+                        break
+                elif ev.key == pygame.K_ESCAPE:
+                    break
+                
+                
+def storyHelpScreen():
+    while True:
+        pygame.init()
+        pygame.display.set_caption("Story Of the Game")
+        res = (shooter.WIDTH, shooter.HEIGHT)
+        screen = pygame.display.set_mode(res) 
+        bg = pygame.image.load("assets/controls.png")
         new = pygame.transform.scale(bg, (1280, 720))
         screen.blit(new, (0, 0))
         pygame.display.update()
@@ -38,5 +59,7 @@ def controlHelpScreen():
         # next page
         for ev in pygame.event.get():     
             if ev.type == pygame.KEYDOWN:
-                if ev.key == pygame.K_RIGHT:
-                    
+                if ev.key == pygame.K_LEFT:
+                    return False # return false will not break the control help loop.
+                elif ev.key == pygame.K_ESCAPE:
+                    return True # return true will break the control help loop as well
